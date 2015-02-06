bc54106e8f986c8db6a0f0650544aae06bbe202c
Chris Fuhrman <chrisfuhrman@gmail.com>
2015-02-06 11:06:05 -0600

    updated

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -18,7 +18,6 @@ class PostTableSeeder extends Seeder
 				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-	        $post->user_id = $user_id;
 
 	        $post->save();
 		}
